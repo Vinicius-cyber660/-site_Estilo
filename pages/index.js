ababfebaa415dfa3ebc864ce537bf8e90aa9258a
Vinicius-cyber660 <winchestersam660@gmail.com>
2022-08-02 01:06:58 -0300

    sem_bootstrap

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ export default function Home(){
   return(
     <>
     <Head>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
       <title>Página Principal</title>
       <meta name="keyworkds" content="Caneca, Personalizado"></meta>
     </Head>
@@ -24,7 +23,6 @@ export default function Home(){
       </div>
     </div>
     <h1 id={styles.maisvend}>Mais Vendidos</h1>
-    <div class="container"> dea</div>
     </>
   )
 }
