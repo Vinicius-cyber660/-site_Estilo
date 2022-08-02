b11a08c748af1eec5ab0c6ee11f1a068cd8576c6
Vinicius-cyber660 <winchestersam660@gmail.com>
2022-08-02 13:28:01 -0300

    resolvido

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,29 +6,41 @@ import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function Navbar(){
     return(<>
-    <div className={styles.cabecalho}>
-    <i className={"fa-solid fa-bars"} id={styles.menu}></i>
-      <Link href="/">
-        <a id={styles.Logo}><img src="/images/logo.png" id={styles.logo}/></a>
-      </Link>
-      <div className={styles.pesquisa}>
-        <InputGroup className={styles.pesquisas}>
-            <Form.Control
-              id={styles.pesquisar}
-              placeholder="pesquisar..."
-              aria-label="Recipient's username"
-              aria-describedby="basic-addon2"
-            />
-            <Button variant="outline-secondary" id="button-addon2" className={styles.botão}>
-              <i className={"fa-solid fa-magnifying-glass"} id={styles.lupa}></i>
-            </Button>
-          </InputGroup>
+    <div id={styles.navbarzao}>
+      <div id={styles.menucel}>
+        <i className={"fa-solid fa-bars"}></i>
+      </div>
+
+      <div id={styles.logao}>
+        <div id={styles.logo10}>
+          <Link href="/">
+            <a id={styles.Logo}><img src="/images/logo.png" id={styles.logo}/></a>
+          </Link>
+        </div>
+      </div>
+
+      <div id={styles.pesquisao}>
+        <div className={styles.pesquisa}>
+          <InputGroup className={styles.pesquisas}>
+              <Form.Control
+                id={styles.pesquisar}
+                placeholder="pesquisar..."
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="outline-secondary" id="button-addon2" className={styles.botão}>
+                <i className={"fa-solid fa-magnifying-glass"} id={styles.lupa}></i>
+              </Button>
+            </InputGroup>
+        </div>
       </div>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"/>
-      <div id={styles.icones}>
-        <i className={"fa-solid fa-circle-user fa-1x"}></i>
-        <i className={"fa-solid fa-cart-shopping"}></i>
-      </div>     
+      <div id={styles.iconesao}>
+        <div id={styles.icones}>
+          <i className={"fa-solid fa-circle-user fa-1x"}></i>
+          <i className={"fa-solid fa-cart-shopping"}></i>
+        </div> 
+      </div>
     </div>
     <div id={styles.nav}>
       <div id={styles.category}>
