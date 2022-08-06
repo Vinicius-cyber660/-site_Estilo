0b61e65dee91b4f72b61ecb9c18d97d5b8865e18
Vinicius-cyber660 <winchestersam660@gmail.com>
2022-08-06 18:10:17 -0300

    pagination

--- components/ProductsSingle.js
+++ components/ProductsSingle.js
@@ -2,11 +2,12 @@ import styles from '../styles/Produtos.module.css'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Link from 'next/link'
 
 export default function ProductsSingle(){
     return(
         <Col xl={3} xs={6}>
-            <div className={styles.Pmaisvendidos}>
+            <Link href="/products/herois/Urso Pô">
                 <div className={styles.produto}>
                     <img src="/images/550x550.png"/>
                     <h4>Caneca Miranha</h4>
@@ -14,7 +15,7 @@ export default function ProductsSingle(){
                     <p>até <strong>3x</strong> de <strong>R$ 9,33</strong> sem juros</p>
                     <Button variant="success" className={styles.comprar}>Comprar</Button>
                 </div>
-            </div>
+            </Link>
         </Col>
     )
 }
--- pages/carrinho.js
+++ pages/carrinho.js
@@ -1,19 +1,10 @@
 import styles from '../styles/Produtos.module.css'
 import Button from 'react-bootstrap/Button';
+import Link from 'next/link'
 
 export default function carrinho(){
     return(<>
     <h1>Carrinho</h1>
-    <div className={styles.Pmaisvendidos}>
-        <a href="/">
-            <div className={styles.produto}>
-                <img src="/images/550x550.png"/>
-                <h4>Caneca Miranha</h4>
-                <h3 className={styles.preço}>R$28,00</h3>
-                <p>até <strong>3x</strong> de <strong>R$ 9,33</strong> sem juros</p>
-                <Button variant="success" className={styles.comprar}>Comprar</Button>
-            </div>
-        </a>
-    </div>
+    
     </>)
 }
