7cb579d0f25b9bbb872d891daeb910abe458b369
Vinicius-cyber660 <winchestersam660@gmail.com>
2022-08-06 00:29:01 -0300

    component carousel

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Carousel from 'react-bootstrap/Carousel';
 import 'react-multi-carousel/lib/styles.css';
-import ProductsCarousel from '../components/productCarousel';
+import ProductsCarousel from '../components/ProductCarousel';
 
 export default function Home(){
   return(
