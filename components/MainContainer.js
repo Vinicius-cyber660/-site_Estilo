4fd2dfb7027e5fd8a2642056731a8387d41e3276
Vinicius-cyber660 <winchestersam660@gmail.com>
2022-08-19 16:22:27 -0300

    teste whats

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -10,12 +10,12 @@ export default function MainContainer({children}){
     <div className={styles.container}>
         {children}
     </div>
-    <a href="https://api.whatsapp.com/send?phone=5512982729709&text=Estilo%20Criação%20a%20seu%20dispor"
-        target="_blank"
-        rel=""
-        id={styles.whats2}>
-        <img id={styles.whatsimage} src="/images/whats.png" alt="Contato pelo WhatsApp" />
-    </a>
+    <Link href="https://api.whatsapp.com/send?phone=5512982729709&text=Estilo%20Criação%20a%20seu%20dispor">
+        <a target="_blank"
+            id={styles.whats2}>
+            <img id={styles.whatsimage} src="/images/whats.png" alt="Contato pelo WhatsApp" />
+        </a>
+    </Link>
     <Footer/>
     </>
     )
