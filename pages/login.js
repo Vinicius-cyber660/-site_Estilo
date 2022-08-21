2a026539e26a020624b96e6b3470fc9f9ed51e4c
Vinicius-cyber660 <winchestersam660@gmail.com>
2022-08-21 15:02:24 -0300

    teste login

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,20 @@
+import styles from '../styles/Login.module.css'
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+
 export default function login(){
-    return(<>
-    <h1>Login</h1>
-    </>
+    const responseGoogle = (response) => {
+        console.log(response);
+    };
+    return(
+    <div id={styles.login}>
+        <h1>Entrar com</h1>
+        <GoogleLogin 
+        clientId="679462750159-7ragf3niqd3bn0629tmpttj18o3ehel2.apps.googleusercontent.com"
+        buttonText="Continuar com o Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        />
+    </div>
      )
 }
