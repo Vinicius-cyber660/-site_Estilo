9864d2468a0a17005f671d52e50ba6134d76c7b3
Vinicius-cyber660 <winchestersam660@gmail.com>
2022-08-22 13:28:26 -0300

    tentativa login 2

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,28 @@
 import styles from '../styles/Login.module.css'
 import React from 'react';
-import GoogleLogin from 'react-google-login';
+import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function login(){
-    const responseGoogle = (response) => {
-        console.log(response);
-    };
-    return(
-    <div id={styles.login}>
-        <h1>Entrar com</h1>
-        <GoogleLogin 
-        clientId="679462750159-7ragf3niqd3bn0629tmpttj18o3ehel2.apps.googleusercontent.com"
-        buttonText="Continuar com o Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        />
-    </div>
+    return(<>
+            <GoogleOAuthProvider  clientId = "679462750159-7ragf3niqd3bn0629tmpttj18o3ehel2.apps.googleusercontent.com">
+
+            <div id={styles.login}>
+                <h1>Entrar com</h1>
+                <meta name="referrer" content="origin"></meta>
+                <GoogleLogin
+                onSuccess = { credentialResponse  =>  { 
+                    console . log ( credentialResponse ) ; 
+                  } } 
+                  onError = { ( )  =>  { 
+                    console . log ( 'Falha no Login' ) ; 
+                  } } >
+
+                    
+                </GoogleLogin>
+
+            </div>
+            </GoogleOAuthProvider>
+            </>
      )
 }
