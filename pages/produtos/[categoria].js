e934ba8d6f2cf512e2648535ee4cc4a2dbb4437a
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-09-01 18:05:02 -0300

    tentativa 4

--- pages/produtos/[categoria].js
+++ pages/produtos/[categoria].js
@@ -48,11 +48,11 @@ export default function Categoria(  {item, products}  ){
     <div id={styles.corpo}>
         <Row>
             {
-            products.map((_produto, index) => (
-                <Col xs={12} sm={6} md={6} lg={3} xl={3} key={index}>
-                    <ProductsSingle product={_produto}/>
-                </Col>
-            ))
+                products.map((_produto, index) => {
+                    <Col xs={12} sm={6} md={6} lg={3} xl={3}>
+                        <ProductsSingle product={_produto} key={index}/>
+                    </Col>
+                })
             }
         </Row>
     </div>
